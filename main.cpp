17ff2217defe53d5b81b6ea093efac929ba17335
Andrew Drost <55260245+MasterZuse@users.noreply.github.com>
2024-09-01 14:30:10 -0500

    First full render

--- main.cpp
+++ main.cpp
@@ -18,27 +18,44 @@ int main() {
     shared_ptr<Material> bronzeMetal = make_shared<Metal>(color(0.8, 0.4, 0.1), 0.6);
     shared_ptr<Material> glass = make_shared<Dielectric>(1.5);
 
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, groundMat));
-    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5, blueMat));
-    world.add(make_shared<sphere>(point3(-1.1, -0.4, -0.8), 0.1, greenMat));
-    world.add(make_shared<sphere>(point3(-1.75, -0.3, -2.25), 0.2, redMat));
-    world.add(make_shared<sphere>(point3(3.0, 0.15, -4.0), 0.75, whiteMetal));
-    world.add(make_shared<sphere>(point3(1.25, 0, -0.9), 0.5, bronzeMetal));
-    world.add(make_shared<sphere>(point3(-1.75, 0.05, -1.5), 0.6, glass));
-    
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, groundMat));
+    world.add(make_shared<sphere>(point3(-3.1, 1.0, -1.0), 1.0, blueMat));
+    world.add(make_shared<sphere>(point3(-0.75, 0.5, 2.0), 0.5, greenMat));
+    world.add(make_shared<sphere>(point3(-1.95, 0.75, -4.25), 0.75, redMat));
+    world.add(make_shared<sphere>(point3(1.25, 0.75, 3.0), 0.75, whiteMetal));
+    world.add(make_shared<sphere>(point3(6, 1.75, -4.5), 1.75, bronzeMetal));
+    world.add(make_shared<sphere>(point3(-0.25, 1.25, -0.25), 1.25, glass));
+
+    for (int z = -20; z <= 6; z++) {
+        int w = 4 + (6 - z) / 1.5;
+        for (int x = -w; x <= w; x++) {
+            float m = randomFloat();
+            float r = randomFloat(0.1, 0.3);
+            point3 pos = point3(x + randomFloat(-0.75, 0.75), r, z + randomFloat(-0.75, 0.75));
+            shared_ptr<Material> mat;
+            if (m < 0.65) {
+                mat = make_shared<Lambertian>(color::random(0.25, 1.0));
+            } else if (m < 0.95) {
+                mat = make_shared<Metal>(color::random(0.5, 1.0), randomFloat(0.1, 0.75));
+            } else {
+                mat = make_shared<Dielectric>(randomFloat(1.3, 1.7));
+            }
+            world.add(make_shared<sphere>(pos, r, mat));
+        }
+    }
 
     Camera camera;
 
     camera.aspectRatio = 16.0 / 9.0;
-    camera.imageWidth = 600;
-    camera.samplesPerPixel = 100;
-    camera.maxDepth = 20;
-    camera.fov = 80;
-    camera.lookFrom = point3(-1.5, 1.5, 0.5);
-    camera.lookAt = point3(0, 0, -1);
+    camera.imageWidth = 800;
+    camera.samplesPerPixel = 400;
+    camera.maxDepth = 40;
+    camera.fov = 60;
+    camera.lookFrom = point3(0, 3, 5);
+    camera.lookAt = point3(0, 0, 0);
     camera.vup = point3(0, 1, 0);
     camera.defocusAngle = 0.5;
-    camera.focusDist = 10;
+    camera.focusDist = 6;
 
     camera.render(world);
 
